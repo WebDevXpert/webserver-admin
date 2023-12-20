fadbf978c553065929ddafdbcd45bde4e237a922
hamza038 <hamzatahir6848@gmail.com>
2023-12-20 14:20:49 +0500

    WEBSERVER-1 || enable dark and lightmode

--- components/RecentOrders.jsx
+++ components/RecentOrders.jsx
@@ -4,22 +4,22 @@ import { FaShoppingBag } from 'react-icons/fa';
 
 const RecentOrders = () => {
   return (
-    <div className='w-full col-span-1 relative lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-white overflow-scroll'>
+    <div className='w-full col-span-1 relative lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-white overflow-scroll dark:bg-light-gray dark:text-white'>
       <h1>Recent Orders</h1>
       <ul>
         {data.map((order, id) => (
           <li
             key={id}
-            className='bg-gray-50 hover:bg-gray-100 rounded-lg my-3 p-2 flex items-center cursor-pointer'
+            className='bg-gray-50 hover:bg-gray-100 rounded-lg my-3 p-2 flex items-center cursor-pointer dark:bg-light-gray dark:text-white'
           >
-            <div className='bg-purple-100 rounded-lg p-3'>
-              <FaShoppingBag className='text-purple-800' />
+            <div className='bg-purple-100 rounded-lg p-3 dark:bg-light-gray dark:text-white'>
+              <FaShoppingBag className='text-purple-800 dark:bg-light-gray dark:text-white' />
             </div>
             <div className='pl-4'>
-              <p className='text-gray-800 font-bold'>${order.total}</p>
-              <p className='text-gray-400 text-sm'>{order.name.first}</p>
+              <p className='text-gray-800 font-bold dark:bg-light-gray dark:text-white'>${order.total}</p>
+              <p className='text-gray-400 text-sm dark:bg-light-gray dark:text-white'>{order.name.first}</p>
             </div>
-            <p className='lg:flex md:hidden absolute right-6 text-sm'>{order.date}</p>
+            <p className='lg:flex md:hidden absolute right-6 text-sm dark:bg-light-gray dark:text-white'>{order.date}</p>
           </li>
         ))}
       </ul>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,20 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark": '#2C3535',
+        "medium": '#323846',
+        "light-gray": '#404349',
+        "Hold": '#E8AD0F',
+        "Completed": '#289124',
+        "Processing": '#249173',
+      }
+    },
   },
   plugins: [],
 };
+
+// #3D5156
