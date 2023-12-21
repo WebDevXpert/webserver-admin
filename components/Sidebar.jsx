0539ede3eaaebf0d58aead24ad6c9dd11cef48e8
hamza038 <hamzatahir6848@gmail.com>
2023-12-21 13:00:51 +0500

    WEBSERVER-3 || sidebar fixes

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -2,43 +2,48 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { RxSketchLogo, RxDashboard, RxPerson } from 'react-icons/rx';
-import { HiOutlineShoppingBag } from 'react-icons/hi';
-import { FiSettings } from 'react-icons/fi';
-
+import { RiLoginBoxFill } from "react-icons/ri";
+import { FaWpforms } from "react-icons/fa";
+import { RiBillLine } from "react-icons/ri";
 const Sidebar = ({ children }) => {
   return (
     <div className='flex dark:bg-dark dark:text-white'>
-      <div className='fixed w-20 h-screen p-4 bg-white dark:bg-dark dark:text-white border-r-[1px] flex flex-col justify-between'>
-        <div className='flex flex-col items-center dark:bg-dark dark:text-white'>
+      <div className='sticky top-0 w-20 h-screen p-4 pb-10 bg-white dark:bg-dark dark:text-white border-r-[1px] flex flex-col justify-between'>
+        <div className='flex flex-col items-center dark:bg-dark dark:text-white text-center'>
           <Link href='/'>
             <div className='bg-purple-800 text-white p-3 rounded-lg inline-block dark:bg-dark dark:text-white'>
-              <RxSketchLogo size={20} />
+              <RxSketchLogo size={35} />
             </div>
           </Link>
           <span className='border-b-[1px] border-gray-200 w-full p-2'></span>
           <Link href='/'>
-            <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block dark:bg-dark dark:text-white'>
-              <RxDashboard size={20} />
+            <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block dark:bg-dark dark:text-white text-center'>
+              <RxDashboard size={35} className='w-full' />
+              <h1 className='text-xs'>Home</h1>
             </div>
           </Link>
-          <Link href='/RegistrationForm'>
-            <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block dark:bg-dark dark:text-white'>
-              <RxPerson size={20} />
+          {/* <Link href='/RegistrationForm'>
+            <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block dark:bg-dark dark:text-white text-center'>
+              <RxPerson size={35} className='w-full' />
+              <h1 className='text-xs'>Register</h1>
             </div>
-          </Link>
+          </Link> */}
           <Link href='/LoginForm'>
             <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block dark:bg-dark dark:text-white'>
-              <RxPerson size={20} />
+              <RiLoginBoxFill size={35} className='w-full' />
+              <h1 className='text-xs'>Login</h1>
             </div>
           </Link>
           <Link href='/OnboardForm'>
             <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block dark:bg-dark dark:text-white'>
-              <HiOutlineShoppingBag size={20} />
+              <FaWpforms size={40} className='w-full' />
+              <h1 className='text-xs'>Site Onboard</h1>
             </div>
           </Link>
           <Link href='/BillingForm'>
             <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block dark:bg-dark dark:text-white'>
-              <FiSettings size={20} />
+              <RiBillLine size={35} className='w-full' />
+              <h1 className='text-xs'>Bill Input</h1>
             </div>
           </Link>
         </div>
