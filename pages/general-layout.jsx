b3586c66cbfd2ff856b21d84198743f41f0d5e28
mhamzaailyas <mhamzaa1947@gmail.com>
2024-01-03 14:49:39 +0500

    WEBSERVER-11 || Resolve Errors of Working on authentication for Logged in User  and Home Page Reidrecting to login if user is not logged-in - Working has been completed

--- pages/general-layout.jsx
+++ pages/general-layout.jsx
@@ -1,7 +1,27 @@
+import { useSession, getSession } from 'next-auth/react';
 import Header from '@/components/Header';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const GeneralLayout = ({ children }) => {
+    const { data: session, status } = useSession();
+    const router = useRouter();
+
+    useEffect(() => {
+        const checkAuth = async () => {
+            const userSession = await getSession();
+            if (!userSession && !['/login', '/register'].includes(router.pathname)) {
+                router.replace('/login');
+            }
+        };
+
+        checkAuth();
+    }, [session, router]);
+
+    if (status === 'loading') {
+        return <p>Loading...</p>;
+    }
+
     return (
         <>
             <Header />
