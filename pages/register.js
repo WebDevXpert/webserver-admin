7f67615663f2afb8dfac02cb06d67c590dd03e1d
WebDevXpert <aliharis2739@gmail.com>
2024-01-04 09:58:08 +0500

    MAIN || updated url

--- pages/register.js
+++ pages/register.js
@@ -99,7 +99,7 @@ export default function Register() {
                     <div className='title'>
                         <h1 className='text-gray-800 text-4xl font-bold py-4'>Register</h1>
                         <p className='w-3/4 mx-auto text-gray-400'>
-                            Get in touch with us
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, officia?
                         </p>
                     </div>
 
