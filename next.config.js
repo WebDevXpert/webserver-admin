c8b0505ed0fb2cd60b955166c3f8087971459b82
WebDevXpert <aliharis2739@gmail.com>
2024-01-07 20:47:36 +0500

    MAIN || url fixez for deployment

--- next.config.js
+++ next.config.js
@@ -8,8 +8,8 @@ const nextConfig = {
     GITHUB_SECRET: '63b9f52bd12f05ae15c53f64e14b877850c2ca91',
     JWT_SECRET: 'khgfkhfhsbdshbdshbdifvbdsh',
     MONGO_URL: 'mongodb+srv://TastyDelights:TastyDelights@cluster0.rezrvcy.mongodb.net/WebServer',
-    // NEXT_PUBLIC_API_BASE_URL:'https://webserver-admin.vercel.app'
-    NEXT_PUBLIC_API_BASE_URL:'https://webserveradmin.vercel.app'
+    NEXT_PUBLIC_API_BASE_URL:'http://localhost:3000'
+    // NEXT_PUBLIC_API_BASE_URL:'https://webserveradmin.vercel.app'
   },
 }
 module.exports = nextConfig;
