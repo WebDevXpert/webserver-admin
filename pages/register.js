ee723c24af18d00b83a9d737013eff6012b76abf
WebDevXpert <aliharis2739@gmail.com>
2024-02-05 16:46:58 +0500

    MAIN || added cors policy

--- pages/register.js
+++ pages/register.js
@@ -48,6 +48,7 @@ export default function Register() {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(values),
+                mode: "cors"
             };
 
             // await fetch(`${apiUrl}/api/auth/signup`, options)
