a073ac347b523cb53973217ff156a1c4a4338361
Muhammad Hamza Ilyas <77743184+mhamzaailyas@users.noreply.github.com>
2024-02-10 14:14:38 +0500

    WEBSERVER-24 || Replace the diamond on the navigation bar with the logo, changes border color around the KPI values In dark mode

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   const { data: session } = useSession();
 
   return (
-    <div className='flex justify-between px-4 pt-4 dark:bg-medium dark:text-white py-3 '>
+    <div className='bg-gray flex justify-between px-4 pt-4 dark:bg-medium dark:text-white py-3 '>
       <h2 className='font-bold text-2xl'>Dashboard</h2>
       {session?.user ? (
         <div className='flex flex-1 items-center justify-end'>
--- layout/layout.js
+++ layout/layout.js
@@ -1,23 +1,29 @@
-import "../styles/Layout.module.css"
-
+import { useSession } from "next-auth/react";
 
 export default function Layout({ children }) {
+    const { data: session } = useSession();
+
+    // When dark mode is enabled
+    const darkModeEnabled = session?.user?.darkMode;
+
+    // dark mode status
+    const layoutClasses = darkModeEnabled
+        ? "flex h-full bg-gray"
+        : "flex h-full light-gray";
+
+    const containerClasses = darkModeEnabled
+        ? "m-auto bg-white rounded-md w-3/5 h-4/7 grid lg:grid-cols-2"
+        : "m-auto light-gray rounded-md w-3/5 h-4/7 grid lg:grid-cols-2";
+
     return (
-        <div className="flex h-full bg-blue-400">
-            <div className="m-auto bg-slate-50 rounded-md w-3/5 h-4/7 grid lg:grid-cols-2">
-                <div className="imgStyle">
-                    <div className="cartoonImg">
-                    </div>
-                    <div className="cloud_one"></div>
-                    <div className="cloud_two"></div>
-                </div>
-                <div className="right flex flex-col justify-evenly">
+        <div className={layoutClasses}>
+            <div className={containerClasses}>
+                <div className="flex flex-col justify-evenly">
                     <div className="text-center py-15">
                         {children}
                     </div>
                 </div>
             </div>
-
         </div>
-    )
+    );
 }
