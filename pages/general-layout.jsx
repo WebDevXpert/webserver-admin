2c7e92e2d8008249c035deb4d364337a97e0837a
WebDevXpert <aliharis2739@gmail.com>
2024-02-15 19:56:19 +0500

    WEBSERVER-main || general layout url fixes

--- pages/general-layout.jsx
+++ pages/general-layout.jsx
@@ -7,16 +7,18 @@ const GeneralLayout = ({ children }) => {
     const { data: session, status } = useSession();
     const router = useRouter();
 
+    const checkAuth = async () => {
+        const userSession = await getSession();
+        // if (!userSession && !['/login', '/register'].includes(router.pathname)) {
+        if (!userSession) {
+            router.push('/login');
+        }
+    };
     useEffect(() => {
-        const checkAuth = async () => {
-            const userSession = await getSession();
-            if (!userSession && !['/login', '/register'].includes(router.pathname)) {
-                router.replace('/login');
-            }
-        };
 
         checkAuth();
-    }, [session, router]);
+    // }, [session, router]);
+    }, []);
 
     if (status === 'loading') {
         return <p>Loading...</p>;
