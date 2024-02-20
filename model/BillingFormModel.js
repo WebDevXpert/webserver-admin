d07b3ecc17b93822134027e33dd418a04fe2d8fc
Muhammad Hamza Ilyas <77743184+mhamzaailyas@users.noreply.github.com>
2024-02-20 12:52:53 +0500

    WEBSERVER-30 || Fixed issue and fecthed list account number and billing types from backend in Billing form

--- model/BillingFormModel.js
+++ model/BillingFormModel.js
@@ -1,39 +1,21 @@
-const billingFormSchema = {
-    accountNumber: { type: Number, required: true },
-    billType: { type: String, required: true },
-    serviceStartDate: { type: Date, required: true },
-    serviceEndDate: { type: Date, required: true },
-    billAmount: { type: Number, required: true },
-    usageAmount: { type: Number, required: true },
-    engineeringUnit: { type: String, required: true },
-};
+import mongoose from 'mongoose';
 
-export default billingFormSchema;
+let BillingFormModel;
 
+try {
+    BillingFormModel = mongoose.model('BillingForm');
+} catch (error) {
+    const billingFormSchema = new mongoose.Schema({
+        accountNumber: { type: String, required: true },
+        billType: { type: String, required: true },
+        serviceStartDate: { type: Date, required: true },
+        serviceEndDate: { type: Date, required: true },
+        billAmount: { type: Number, required: true },
+        usageAmount: { type: Number, required: true },
+        engineeringUnit: { type: String, required: true },
+    });
 
+    BillingFormModel = mongoose.model('BillingForm', billingFormSchema);
+}
 
-
-
-
-
-// import mongoose from 'mongoose';
-
-// let BillingFormModel;
-
-// try {
-//     BillingFormModel = mongoose.model('BillingForm');
-// } catch (error) {
-//     const billingFormSchema = new mongoose.Schema({
-//         accountNumber: { type: Number, required: true },
-//         billType: { type: String, required: true },
-//         serviceStartDate: { type: Date, required: true },
-//         serviceEndDate: { type: Date, required: true },
-//         billAmount: { type: Number, required: true },
-//         usageAmount: { type: Number, required: true },
-//         engineeringUnit: { type: String, required: true },
-//     });
-
-//     BillingFormModel = mongoose.model('BillingForm', billingFormSchema);
-// }
-
-// export default BillingFormModel;
+export default BillingFormModel;
--- pages/general-layout.jsx
+++ pages/general-layout.jsx
@@ -1,22 +1,52 @@
 import { useSession, getSession } from 'next-auth/react';
-import Header from '@/components/Header';
-import React, { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 
 const GeneralLayout = ({ children }) => {
+    const [showPopup, setShowPopup] = useState(false);
     const { data: session, status } = useSession();
     const router = useRouter();
 
     useEffect(() => {
         const checkAuth = async () => {
             const userSession = await getSession();
-            if (!userSession && !['/login', '/register'].includes(router.pathname)) {
+            // if (!userSession && !['/login', '/register'].includes(router.pathname)) {
+            if (!userSession) {
                 router.replace('/login');
+            } else if (userSession) {
+                const expiresAt = new Date(userSession.expires * 1000);
+                const now = new Date();
+                const timeUntilExpiration = expiresAt - now;
+
+                // Show popup 2 minutes before session expiration
+                if (timeUntilExpiration > 0 && timeUntilExpiration <= 2 * 60 * 1000) {
+                    setShowPopup(true);
+                }
+
+                // Expire session after 5 minutes
+                if (timeUntilExpiration <= 0) {
+                    await router.replace('/login');
+                }
+
+                // // Show popup 5 minutes before session expiration
+                // if (timeUntilExpiration > 0 && timeUntilExpiration <= 5 * 60 * 1000) {
+                //     setShowPopup(true);
+                // }
+
+                // // Expire session after 9 hours
+                // if (timeUntilExpiration <= 0) {
+                //     await router.replace('/login');
+                // }
             }
         };
 
         checkAuth();
     }, [session, router]);
+
+    const handleLogout = async () => {
+        await fetch('/api/auth/logout');
+        await router.replace('/login');
+    };
 
     if (status === 'loading') {
         return <p>Loading...</p>;
@@ -24,7 +54,12 @@ const GeneralLayout = ({ children }) => {
 
     return (
         <>
-            {/* <Header /> */}
+            {showPopup && (
+                <div className="popup">
+                    <p>Please login again to renew your session.</p>
+                    <button onClick={handleLogout}>Logout</button>
+                </div>
+            )}
             <main>{children}</main>
         </>
     );
